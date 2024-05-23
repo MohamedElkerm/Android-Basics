c73cc59555d6d056bb7571236c1926693973621f
Mohamed Elkerm <mohamedelkerm584@gmail.com>
2024-05-23 23:09:20 +0300

    add botttom nav bar

--- app/src/main/java/com/example/myapplication/MainActivity3.kt
+++ app/src/main/java/com/example/myapplication/MainActivity3.kt
@@ -3,11 +3,16 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.fragment.app.FragmentTransaction
 import com.example.myapplication.databinding.ActivityMain3Binding
 
 class MainActivity3 : AppCompatActivity() {
     
     lateinit var  binding :ActivityMain3Binding
+
+    private val trans = supportFragmentManager.beginTransaction()
+    val fragOne = OneFrag()
+    val fragTwo = TwoFrag()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -15,8 +20,44 @@ class MainActivity3 : AppCompatActivity() {
         setContentView(binding.root)
 
         fabCallBack()
+        initFrag()
+        addNavAction()
     }
 
+    private  fun initFrag(){
+        trans.add(R.id.fragId , fragTwo)
+        trans.commit()
+    }
+
+
+    private fun addNavAction(){
+        binding.bottomNavBar.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.pageHome -> {
+                    trans.remove(fragTwo)
+                    trans.add(R.id.fragId , fragOne)
+                    trans.commit()
+                    true
+                }
+
+                R.id.pageMusic -> {
+                    trans.remove(fragOne)
+                    trans.add(R.id.fragId , fragTwo)
+                    trans.commit()
+                    true
+                }
+
+                R.id.pageProfile -> {
+                    trans.remove(fragTwo)
+                    trans.add(R.id.fragId , fragOne)
+                    trans.commit()
+                    true
+                }
+                else -> false
+            }
+
+        }
+    }
     private  fun fabCallBack (){
         binding.FAB.setOnClickListener {
             Toast.makeText(this , "Hello Mohamed Elkerm" , Toast.LENGTH_SHORT).show()
