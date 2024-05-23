33ceceb62b279a637afdbbd309e26ed6b3dfde24
Mohamed Elkerm <mohamedelkerm584@gmail.com>
2024-05-23 21:02:05 +0300

    add fab in activity 3

--- app/src/main/java/com/example/myapplication/MainActivity3.kt
+++ app/src/main/java/com/example/myapplication/MainActivity3.kt
@@ -2,10 +2,24 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivityMain3Binding
 
 class MainActivity3 : AppCompatActivity() {
+    
+    lateinit var  binding :ActivityMain3Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main3)
+
+        binding = ActivityMain3Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        fabCallBack()
+    }
+
+    private  fun fabCallBack (){
+        binding.FAB.setOnClickListener {
+            Toast.makeText(this , "Hello Mohamed Elkerm" , Toast.LENGTH_SHORT)
+        }
     }
 }
